db76abaf20b9b410758eadcacccbf656b974c9c7
Darkriley <ryanmae.pro@gmail.com>
2021-12-23 09:34:38 +0100

    js

---
+++ app.js
@@ -0,0 +1,24 @@
+
+window.addEventListener('click', (e) => {
+    // console.log(e);
+
+    const rond = document.createElement('div');
+    rond.className = 'clickAnim';
+    rond.style.top = `${e.pageY - 50}px`;
+    rond.style.left = `${e.pageX - 50}px`;
+    document.body.appendChild(rond);
+
+    setTimeout(() => {
+        rond.remove();
+    }, 1500)
+})
+function toggleMenu () {  
+    const navbar = document.querySelector('.navbar');
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', (e) => {    
+      navbar.classList.toggle('show-nav');
+    });    
+  }
+  toggleMenu();
+  
+  
